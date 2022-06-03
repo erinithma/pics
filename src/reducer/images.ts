dae2733bfb4cc1d9a794077520257c6736a13078
Ivan <ivan.mikhaylov@gost-group.com>
2022-06-03 13:41:05 +0300

    fix

--- src/reducer/images.ts
+++ src/reducer/images.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { Api } from "../common/api";
 import { Image } from "../common/types";
 
